5408d5c8b9e8fe941c9856c0bada1f41457ade97
KMCCAL200 <ninja-star@hotmail.co.uk>
2013-10-01 10:51:22 +0000

    update

--- GP2BaseCode/GameApplication/GameApplication.h
+++ GP2BaseCode/GameApplication/GameApplication.h
@@ -1,18 +1,52 @@
-//header guard, so this file is only complied once
+//header guard, so this file is only compiled once
 #pragma once
 
 #include <string>
 
 using namespace std;
 
-//Forward declaration of our window interface
+//Forward decleration of our Window Interface
 class IWindow;
+class IRenderer;
 
-//structure for holding gameOptions, this will be loaded from config files
+//Structure for holding GameOptions, this will be loaded from config files
 struct GameOptionsDesc
 {
 	wstring gameName;
 	int width;
 	int height;
 	bool fullscreen;
-}
+};
+
+//we have this here so we don't need to prefix all
+//standard library types with std::
+using namespace std;
+
+//Our Game Application class
+class CGameApplication
+{
+public:
+	CGameApplication(void);
+	//virtual deconstructor, so this class can be overriden
+	virtual ~CGameApplication(void);
+	//virtual function, can be overriden
+	virtual bool init();
+	void run();
+	//virtual function, can be overriden
+	virtual void render();
+	//virtual function, can be overriden
+	virtual void update();
+private:
+	bool parseConfigFile();
+	bool initInput();
+	bool initGame();
+	bool initGraphics();
+	bool initPhysics();
+
+	bool initWindow();
+private:
+	IWindow * m_pWindow;
+	IRenderer * m_pRenderer;
+	GameOptionsDesc m_GameOptionDesc;
+	wstring m_ConfigFileName;
+};
